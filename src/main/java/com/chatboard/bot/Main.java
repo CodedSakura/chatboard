b258b410708f060992839fa9e88eb95d1502736f
Tomass Ruza <tomass.ruza@gmail.com>
2019-01-01 23:31:07 +0200

    Test Commit

--- src/main/java/com/chatboard/bot/Main.java
+++ src/main/java/com/chatboard/bot/Main.java
@@ -7,7 +7,7 @@ import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        new JDABuilder(AccountType.BOT).setToken(args[0]).build();
+        new JDABuilder(AccountType.BOT).setToken("").build();
     }
-    //
+    
 }
