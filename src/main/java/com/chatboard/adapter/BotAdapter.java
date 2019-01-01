082cb553a4494d5a310f772b282e2c82e7ec8f5c
Tomass Ruza <tomass.ruza@gmail.com>
2019-01-02 00:40:47 +0200

    Added Bot Event Adapter

---
+++ src/main/java/com/chatboard/adapter/BotAdapter.java
@@ -0,0 +1,13 @@
+package com.chatboard.adapter;
+
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class BotAdapter extends ListenerAdapter {
+
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent evt) {
+        
+    }
+
+}
