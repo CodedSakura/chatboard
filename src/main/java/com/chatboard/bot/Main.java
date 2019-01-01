ba9b7b874046c70eef62a44b1d0a2b564c61f0df
Tomass Ruza <tomass.ruza@gmail.com>
2019-01-02 00:11:49 +0200

    Replaced tabs with 4 spaces

--- src/main/java/com/chatboard/bot/Main.java
+++ src/main/java/com/chatboard/bot/Main.java
@@ -6,14 +6,14 @@ import com.chatboard.wrapper.JDAWrapper;
 public class Main {
 	
     public static void main(String... args) {
-    	
-    	String token = args[0];            // Takes the token as an argument
-    	try {
-    		JDAWrapper.initialize(token);  // Initializes the JDA in the JDA wrapper
-    	} catch(Exception e) {
-    		System.exit(-1);
-    	}
-    	
+        
+        String token = args[0];              // Takes the token as an argument
+        try {
+            JDAWrapper.initialize(token);    // Initializes the JDA in the JDA wrapper
+        } catch(Exception e) {
+    	    System.exit(-1);
+        }
+        
     }
     
 }
