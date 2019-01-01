34e87cf64b6fed7e601e5db15f3e67e4f5dd3b2d
Tomass Ruza <tomass.ruza@gmail.com>
2019-01-02 00:06:18 +0200

    Updates related to exception handling

--- src/main/java/com/chatboard/bot/Main.java
+++ src/main/java/com/chatboard/bot/Main.java
@@ -1,15 +1,18 @@
 package com.chatboard.bot;
 
-import javax.security.auth.login.LoginException;
 import com.chatboard.wrapper.JDAWrapper;
 
 /** The main launcher class */
 public class Main {
 	
-    public static void main(String... args) throws LoginException {
+    public static void main(String... args) {
     	
-    	String token = args[0];          // Takes the token as an argument
-    	JDAWrapper.initialize(token);    // Initializes the JDA in the JDA wrapper
+    	String token = args[0];           // Takes the token as an argument
+    	try {
+    		JDAWrapper.initialize(token); // Initializes the JDA in the JDA wrapper
+    	} catch(Exception e) {
+    		System.exit(-1);
+    	}
     	
     }
     
