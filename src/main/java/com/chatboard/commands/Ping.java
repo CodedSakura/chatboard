90092dc92d2a500030b49f96c18ad4e89486b2d2
thephisics101 <thephisics101@gmail.com>
2019-01-02 22:31:12 +0200

    Implement Ping command

--- src/main/java/com/chatboard/commands/Ping.java
+++ src/main/java/com/chatboard/commands/Ping.java
@@ -1,17 +1,16 @@
 package com.chatboard.commands;
 
 import com.chatboard.annotation.*;
+import com.chatboard.commandparser.Command;
+import com.chatboard.wrapper.JDAWrapper;
 
-public class Ping {
-
+public class Ping extends Command {
     private Ping() {}
-    
-    
-    
+
     @Runner
     @NoAdmin
-    public static void runNoArgs() {
-        
+    public void runNoArgs() {
+        this.getTextChannel().sendMessage(String.format("Ping: %dms", JDAWrapper.getJDA().getPing())).queue();
     }
 
 }
