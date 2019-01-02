6d530d63ffab58c6b4010ea6e8d041d7a49aa59b
Tomass Ruza <tomass.ruza@gmail.com>
2019-01-02 15:26:46 +0200

    Updated JUnit test for ParserUtils

--- src/test/java/com/chatboard/commandparser/ParserUtilsTest.java
+++ src/test/java/com/chatboard/commandparser/ParserUtilsTest.java
@@ -1,32 +1,27 @@
 package com.chatboard.commandparser;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-@RunWith(Arquillian.class)
 public class ParserUtilsTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(ParserUtils.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
-
+    
     @Test
     public void parser() {
-        assertArrayEquals(ParserUtils.parser(">command GET"), new Object[] {new Mode("GET")});
-        assertArrayEquals(ParserUtils.parser(">command 1"), new Object[] {1});
-        assertArrayEquals(ParserUtils.parser(">command \"asd\""), new Object[] {"asd"});
-        assertArrayEquals(ParserUtils.parser(">command \"asd qwe\""), new Object[] {"asd qwe"});
-        assertArrayEquals(ParserUtils.parser(">command \"asd\\\" qwe\""), new Object[] {"asd\" qwe"});
-        assertArrayEquals(ParserUtils.parser(">command \"asd\\nqwe\""), new Object[] {"asd\nqwe"});
-        assertArrayEquals(ParserUtils.parser(">command \"asd\\qwe\""), new Object[] {"asd\\qwe"});
+        assertArrayEquals(ParserUtils.parser(">command GET"),               new Object[] {new Mode("GET")});
+        assertArrayEquals(ParserUtils.parser(">command 1"),                 new Object[] {1});
+        assertArrayEquals(ParserUtils.parser(">command \"asd\""),           new Object[] {"asd"});
+        assertArrayEquals(ParserUtils.parser(">command \"asd qwe\""),       new Object[] {"asd qwe"});
+        assertArrayEquals(ParserUtils.parser(">command \"asd\\\" qwe\""),   new Object[] {"asd\" qwe"});
+        assertArrayEquals(ParserUtils.parser(">command \"asd\\nqwe\""),     new Object[] {"asd\nqwe"});
+        assertArrayEquals(ParserUtils.parser(">command \"asd\\qwe\""),      new Object[] {"asd\\qwe"});
+        assertArrayEquals(ParserUtils.parser(">command"),                   new Object[] {});
+        assertArrayEquals(ParserUtils.parser(">command "),                  new Object[] {});
+        assertArrayEquals(ParserUtils.parser(">command   "),                new Object[] {});
+        assertArrayEquals(ParserUtils.parser(">command  1   3"),            new Object[] {1, 3});
+        assertArrayEquals(ParserUtils.parser(">command 1   3"),             new Object[] {1, 3});
+        assertArrayEquals(ParserUtils.parser(">command  1   3  "),          new Object[] {1, 3});
+        
     }
 }
